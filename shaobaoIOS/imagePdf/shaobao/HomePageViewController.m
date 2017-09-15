6038026ccaa5c33c86c34da76201eeaa33a8d9f3
hfqf123 <hfqf123@126.com>
2017-09-15 09:13:49 +0800

    1

--- shaobaoIOS/imagePdf/shaobao/HomePageViewController.m
+++ shaobaoIOS/imagePdf/shaobao/HomePageViewController.m
@@ -7,8 +7,10 @@
 //
 
 #import "HomePageViewController.h"
+#import "MXCycleScrollView.h"
 
-@interface HomePageViewController ()
+@interface HomePageViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property(nonatomic,strong)MXCycleScrollView *cycleScrollView;
 
 @end
 
@@ -34,14 +36,8 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (void)addAds{
+    
 }
-*/
 
 @end
---
+++ shaobaoIOS/imagePdf/shaobao/MXCycleScrollView/MXCycleScrollView.h
@@ -0,0 +1,76 @@
+//
+//  MXCycleScrollView.h
+//  MXScrollViewDemo
+//
+//  Created by msxf on 2017/5/24.
+//  Copyright © 2017年 yellow. All rights reserved.
+//
+
+/*
+ github:https://github.com/iamhmx
+ */
+
+#import <UIKit/UIKit.h>
+#import "MXCycleScrollViewHeader.h"
+
+/**
+ 图片和文字模型
+ */
+@interface MXImageModel : NSObject
+@property (copy, nonatomic) NSString *imageUrl;
+@property (copy, nonatomic) NSString *imageText;
+@end
+
+@protocol MXCycleScrollViewDelegate <NSObject>
+
+@optional
+- (void)clickImageIndex:(NSInteger)index;
+
+@end
+
+typedef void(^MXClickImageHandler)(NSInteger index);
+
+@interface MXCycleScrollView : UIView
+//图片&文字内容
+@property (strong, nonatomic) NSArray <MXImageModel*>* contents;
+//自动滚动间隔时间
+@property (assign, nonatomic) CGFloat delay;
+@property (weak, nonatomic)   id <MXCycleScrollViewDelegate> delegate;
+@property (strong, nonatomic) UIImage *placeholderImage;
+@property (copy, nonatomic)   MXClickImageHandler clickHandler;
+//是否显示pageControl，默认显示
+@property (assign, nonatomic) BOOL hidePageControl;
+//pageControl颜色
+@property (nonatomic, strong) UIColor *pageIndicatorTintColor;
+@property (nonatomic, strong) UIColor *currentPageIndicatorTintColor;
+//pageControl圆点大小
+@property (assign, nonatomic) CGFloat pageControlDotWidth;
+//pageControl圆点间距
+@property (assign, nonatomic) CGFloat pageControlDotMargin;
+//显示文字
+@property (assign, nonatomic) BOOL showText;
+//动画类型，默认无动画
+@property (assign, nonatomic) MXImageAnimation animationType;
+//缩放动画的缩放系数（0~0.9）
+@property (assign, nonatomic) CGFloat scaleRatio;
+
+/**
+ 初始化（用于事先不知道图片数据，一般图片数据有网络请求而来，先设置好视图，然后设置contents属性）
+
+ @param frame 位置
+ @param delay 自动滚动间隔时间
+ @return self
+ */
+- (instancetype)initWithFrame:(CGRect)frame withScrollDelay:(CGFloat)delay;
+
+/**
+ 初始化（用于事先知道图片数据）
+
+ @param frame 位置
+ @param contents 图片地址
+ @param delay 自动滚动间隔时间
+ @return self
+ */
+- (instancetype)initWithFrame:(CGRect)frame withContents:(NSArray<NSString*>*)contents andScrollDelay:(CGFloat)delay;
+
+@end
